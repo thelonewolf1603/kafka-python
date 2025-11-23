c8e7bc25decdd6561a8448cae66c407e009e5f3f
Neeladri Das <neeladridas@Neeladris-MacBook-Air.local>
2025-11-23 12:31:29 +0530

    enhancements

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,11 @@ app = fastapi.FastAPI()
 
 @app.get('/')
 def home():
-    return "welcome to this app tatai!!"
+    return "welcome to this app sujaaata!!"
+
+@app.get('/health')
+def health():
+    return None
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=3000, reload=True)
